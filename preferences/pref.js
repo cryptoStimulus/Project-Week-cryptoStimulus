1f84993477a865b780073c882179ac1a35c9599a
Lucia Brammer <brammerl96@gmail.com>
2020-04-21 14:10:07 -0700

    update to make the function update local storage

---
+++ preferences/pref.js
@@ -0,0 +1,35 @@
+
+const updatingLocalStorage = () => { 
+    const radio = document.querySelectorAll('input');
+
+    // import data for local storage preferences
+    let preferenceData = localStorage.getItem('preferences');
+
+    // if it does not exist, we will need to create array
+    if (!preferenceData){
+        // make an empty array if we do not have preference data 
+        preferenceData = []; 
+    } else {
+        // if we do have it, then parse 
+        preferenceData = JSON.parse(preferenceData);
+    }
+
+    radio.forEach((preferenceButton) => {
+        preferenceButton.addEventListener('click', () => {
+
+            preferenceData.push(event.target.value);  // pushing the value of oiinput radio into our empty array 
+
+
+            const stringifiedPreferenceData = JSON.stringify(preferenceData); //stringifying our updated array
+
+            localStorage.setItem('preferences', stringifiedPreferenceData); //updating local storage
+        });
+    });
+
+};
+
+updatingLocalStorage();
+
+
+
+
