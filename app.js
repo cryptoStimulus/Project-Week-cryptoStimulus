edfc9c709a5bd521e7a435883430ac49ddf251d7
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-22 11:11:36 -0700

    reformatting local storage in login page app.js so that when adding a new user, it refreshes to a new user and overrides other userdata

--- app.js
+++ app.js
@@ -6,36 +6,22 @@ const loginButton = document.getElementById('login-button');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 
-
-
-
 loginButton.addEventListener('click', (event) => {
     event.preventDefault();
     if (email.value === '' || username.value === '') {
         return;
     }
-    else {
+    else { // will reassign the different username once inputted to have a 'fresh' local storage
         let userDataLocalStorage = localStorage.getItem('userData'); //find item in local storage called userData
-        if (!userDataLocalStorage){ // if it does not exist
-            const madeUser = new FormData(form); 
-            const userDataLocalStorage = makeUser(madeUser);
-            const stringed = JSON.stringify(userDataLocalStorage);
-            localStorage.setItem('userData', stringed);
-
-        } else {
-            userDataLocalStorage = JSON.parse(userDataLocalStorage); //if it does exist
-            const updatedUserInfo = new FormData(form);
-
-            userDataLocalStorage = updatedUserInfo; 
-        }
+        if (!userDataLocalStorage) { //if it does not exist, it will set so that way, we can do lines 20-26
+            userDataLocalStorage = 'hello'; 
+            localStorage.setItem('userData', userDataLocalStorage);
+        } 
+        const madeUser = new FormData(form); 
+        userDataLocalStorage = makeUser(madeUser);
+        const stringed = JSON.stringify(userDataLocalStorage);
+        localStorage.setItem('userData', stringed);
         
-        
-        
-
-
-        // import user Data from local storage 
-
-
         window.location.replace ('./preferences/index.html');
     }    
 });
