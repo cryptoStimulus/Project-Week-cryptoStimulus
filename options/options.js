d21e3aae4f59302573f94b7ca9b8582ce6d6559a
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-21 17:36:36 -0700

    created findbycategory file that works, established dom elements to append tomorrow, still working on rendercompanyblock function

--- options/options.js
+++ options/options.js
@@ -1,8 +1,78 @@
-//render a company block 
-    //pull down localStorage named 'options' to get acess to 'optionsArray'
+// ///////////////
+//PROJECT UPDATE FOR US TOMORROW: 
+// we created a findbyCategory function that is working and ready 
+// created elements in the DOM to append to our html elements 
+// currently working on renderCompany Block function, we need to append from text div down 
+// we also pulled both our local storages to be utilized in this file
+
+
+// import other files 
+
+import orgsData from '../data/orgs-data-list.js';
+import findByCategory from 'find-by-category.js';
+
+const parsedPreferences = JSON.parse(localStorage.getItem('preferences')); 
+
+findByCategory(parsedPreferences, orgsData); 
+
+const parsedOptions = JSON.parse(localStorage.getItem('options')); 
+
+
+// for each company that comes through options array, we're filling out our template and creating the block
+ 
+//render a company block function
+export function renderCompanyBlock() { 
+
+    for (let i = 0; i < parsedOptions.length; i++){
+        const company = parsedOptions[i]; 
+
+        // parent company block
+        const companyBlock = document.createElement('section');
+        companyBlock.classList.add('company');
+    
+        // logo div 
+        const logoDiv = document.createElement('div'); 
+        logoDiv.id = 'logo';
+        const companyImage = document.createElement('img');
+        companyImage.src = `../assets/companies/${company.image}`;
+        //append to the logo div
+        logoDiv.appendChild(companyImage);
+        companyBlock.appendChild(logoDiv);
+
+
+        // text div 
+        const textDiv = document.createElement('div'); 
+        textDiv.id = 'company-id';
+        const pName = document.createElement('p'); 
+        pName.textContent = company.name;
+        const pDescription = document.createElement('p');
+        pDescription.textContent = company.description; 
+        //append to the text div 
+        textDiv.appendChild(pName);
+    
+    
+        // category div
+        const categoryDiv = document.createElement('div'); 
+        categoryDiv.id = 'category';
+        const pCategory = document.createElement('p');
+        pCategory.textContent = company.category; 
+    
+        // save div
+        const saveDiv = document.createElement('div'); 
+        saveDiv.id = 'fav-button';
+        const favButton = document.createElement('button');
+        favButton.textContent = 'Favorite'; 
+        
+
+    }
+
+
+
+//pull down localStorage named 'options' to get acess to 'optionsArray'
     //go through 'optionsArray'(a for loop or a forEach)
     // create blocks for each company of code to be childAppended to a parent node called the "company section". 
 
+}
 
 /*
 <section class='company'>
@@ -10,7 +80,7 @@
                 <image>
             </div>
             <div id=text>
-                <p/h1/>name<p/h1/>
+                <p>name<p/>
                 <p>description</p>
             </div>
             <div id='cat'>
