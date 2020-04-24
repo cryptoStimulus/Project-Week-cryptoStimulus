59fe5c9e91400dee1d0822ace62dedf61cf0285b
Lucia Brammer <brammerl96@gmail.com>
2020-04-24 09:48:17 -0700

    fixing javascript bugs

--- data/button-data.js
+++ data/button-data.js
@@ -2,10 +2,10 @@ const buttonOn = [{
     id: 'animal rights',
     src: '../assets/buttons/button_animal-rights-on.png'
 }, {
-    id: 'food bank / shelter',
+    id: 'food bank/ shelter',
     src: '../assets/buttons/button_food-bank-shelter-on.png'
 }, {
-    id: 'service / artist funds',
+    id: 'service/ artist funds',
     src: '../assets/buttons/button_service-artist-funds-on.png'
 }, {
     id: 'social advocacy',
